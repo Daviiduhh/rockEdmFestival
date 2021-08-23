2e7257092d6ed5710b62083c57be8db81b4481f2
Daviiduhh <daviiduhhs@gmail.com>
2021-08-22 22:49:42 -0500

    Se arreglo el problema de compilacion de gulp, el problema era wewp en lugar de webp, se agrego el html y estilos de line up. Se agrego html, js, y estilos de galeria, falta terminar estilos

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { series, src, dest, watch } = require('gulp');
-const sass = require("gulp-sass")(require("node-sass"));
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const concat = require('gulp-concat');
+
+const paths = {
+    imagenes : 'src/img/**/*',
+    scss : 'src/scss/**/*',
+    js : 'src/js/**/*'
+}
 
 function css(done) {
     return src('src/scss/app.scss')
@@ -20,18 +28,39 @@ function mcss(done) {
     done();
 }
 
+function javascript(done) {
+    return src(paths.js)
+        .pipe(concat('bundle.js'))
+        .pipe(dest('./js'))
+    done();
+}
+
 function imagenes (done) {
     return src('src/img/**/*')
         .pipe(imagemin())
         .pipe(dest('./img'))
+
+    done();
+}
+
+function versionWebp (done) {
+    return src('src/img/**/*')
+        .pipe(webp())
+        .pipe(dest('./img'))
+    
     done();
 }
 
 function watchFiles() {
     watch('src/scss/**/*.scss', mcss) //* busaca dentro de la carpeta actual. **/* ya busca todos los archivos de todas las carpetas hijos
+    watch(paths.js, javascript)
 }
 
 exports.css = css;
 exports.mcss = mcss;
 exports.watchFiles = watchFiles;
 exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.javascript = javascript
+
+exports.default = series(javascript, imagenes, versionWebp, watchFiles)
